c01ac090f08e7b9d20ac04485e15d0502fbf10cb
schimen <einesimen@gmail.com>
2021-12-17 14:20:36 +0100

    xmonad.hs add physical screen control

--- xsession/xmonad/xmonad.hs
+++ xsession/xmonad/xmonad.hs
@@ -1,13 +1,17 @@
 import XMonad
-import XMonad.Actions.SpawnOn   (spawnOn, manageSpawn)
-import XMonad.Config.Desktop    (desktopConfig)
-import XMonad.Config.Xfce       (xfceConfig)
-import XMonad.Layout.Fullscreen (fullscreenSupport)
-import XMonad.Layout.NoBorders  (smartBorders)
-import XMonad.Layout.Spacing    (smartSpacingWithEdge)
-import XMonad.Actions.Volume    (lowerVolume, raiseVolume, toggleMute)
-
+import XMonad.Actions.SpawnOn         (spawnOn, manageSpawn)
+import XMonad.Actions.Volume          (lowerVolume, raiseVolume, toggleMute)
+import XMonad.Actions.PhysicalScreens (viewScreen, sendToScreen)
+import XMonad.Config.Desktop          (desktopConfig)
+import XMonad.Config.Xfce             (xfceConfig)
+import XMonad.Layout.Fullscreen       (fullscreenSupport)
+import XMonad.Layout.NoBorders        (smartBorders)
+import XMonad.Layout.Spacing          (spacingRaw, Border(Border), Spacing (smartBorder))
+import Control.Monad                  (void)            
 import qualified Data.Map as M
+import GHC.Base (Bool(True))
+import GHC.Types (Bool(True))
+import XMonad.Config.Prime (Bool(True))
 
 main = do
     xmonad $ fullscreenSupport xfceConfig
@@ -16,36 +20,57 @@ main = do
         , workspaces         = myWorkspaces
         , normalBorderColor  = myNormalBorderColor
         , focusedBorderColor = myFocusedBorderColor
-	, layoutHook         = smartSpacingWithEdge 4 $ smartBorders $ layoutHook xfceConfig
-        , keys               = myKeys        <+> keys desktopConfig
-	--, startupHook        = myStartupHook <+> startupHook xfceConfig
-        , manageHook         = manageSpawn   <+> manageHook  xfceConfig
-	}
+        , layoutHook         = myLayoutHook
+        , keys               = myKeys      <+> keys        desktopConfig
+        , manageHook         = manageSpawn <+> manageHook  xfceConfig
+        --, startupHook        = myStartupHook <+> startupHook xfceConfig
+    }
+
+myLayoutHook = spacingRaw True             -- smartBorder  enabled
+                          (Border 1 2 3 4) -- screenBorder 
+                          True             -- screenBorder enabled
+                          (Border 1 2 3 4) -- windowBorder 
+                          True             -- windowBorder enabled
+                          (smartBorders $ layoutHook xfceConfig)
+
 
 -- keys
-myKeys conf@(XConfig {modMask = modm}) = M.fromList $
-    [ ((modm,                        xK_p  ), spawn "dmenu_run")
-    , ((modm        .|. shiftMask,   xK_p  ), spawn "rofi -show run")
-    , ((modm        .|. shiftMask,   xK_s  ), spawn "xfce4-screenshooter -c -r")
-    , ((modm,                        xK_c  ), spawn restartXmonad)
-    , ((modm,                        xK_q  ), kill)
+myKeys conf@XConfig {modMask = modm} = M.fromList $
+    [ ((modm,                        xK_q  ), kill                        )
+    , ((modm,                        xK_p  ), spawn dmenuCommand          )
+    , ((modm        .|. shiftMask,   xK_p  ), spawn rofiCommand           )
+    , ((modm        .|. shiftMask,   xK_s  ), spawn screenshotCommand     )
+    , ((modm,                        xK_c  ), spawn restartXmonad         )
     , ((mod1Mask    .|. controlMask, xK_t  ), spawn $ XMonad.terminal conf)
-    , ((mod1Mask    .|. controlMask, xK_f  ), spawn "thunar")
-    , ((mod1Mask    .|. controlMask, xK_l  ), spawn "light-locker-command --lock")
-    , ((controlMask,                 xK_F11), spawn "brightnessctl --device=intel_backlight s 10%-")
-    , ((controlMask,                 xK_F12), spawn "brightnessctl --device=intel_backlight s +10%")
-    , ((controlMask .|. shiftMask,   xK_F11), spawn "brightnessctl --device=intel_backlight s 1%-")
-    , ((controlMask .|. shiftMask,   xK_F12), spawn "brightnessctl --device=intel_backlight s +1%")
-    , ((controlMask,                 xK_F1 ), toggleMute     >> return ())
-    , ((controlMask,                 xK_F2 ), lowerVolume 10 >> return ())
-    , ((controlMask,                 xK_F3 ), raiseVolume 10 >> return ())
-    , ((controlMask .|. shiftMask,   xK_F2 ), lowerVolume 1  >> return ())
-    , ((controlMask .|. shiftMask,   xK_F3 ), raiseVolume 1  >> return ())
+    , ((mod1Mask    .|. controlMask, xK_f  ), spawn fileManager           )
+    , ((mod1Mask    .|. controlMask, xK_l  ), spawn lockScreenCommand     )
+    , ((controlMask,                 xK_F11), spawn brightnessDownBig     )
+    , ((controlMask,                 xK_F12), spawn brightnessUpBig       )
+    , ((controlMask .|. shiftMask,   xK_F11), spawn brightnessDownSmall   )
+    , ((controlMask .|. shiftMask,   xK_F12), spawn brightnessUpSmall     )
+    , ((controlMask,                 xK_F1 ), void   toggleMute           )
+    , ((controlMask,                 xK_F2 ), void $ lowerVolume 10       )
+    , ((controlMask,                 xK_F3 ), void $ raiseVolume 10       )
+    , ((controlMask .|. shiftMask,   xK_F2 ), void $ lowerVolume 1        )
+    , ((controlMask .|. shiftMask,   xK_F3 ), void $ raiseVolume 1        )
+    ] ++ physicalScreensKeys modm
 
+physicalScreensKeys modm = 
+    [ ((modm .|. mask, key), f sc) 
+        | (key, sc) <- zip [xK_w, xK_e] [0..]
+        , (f, mask) <- [(viewScreen def, 0), (sendToScreen def, shiftMask)]
     ]
 
--- restart command
-restartXmonad = "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi"
+dmenuCommand        = "dmenu_run"
+rofiCommand         = "rofi -show run"
+screenshotCommand   = "xfce4-screenshooter -c -r "
+restartXmonad       = "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi"
+fileManager         = "thunar"
+lockScreenCommand   = "light-locker-command --lock"
+brightnessDownBig   = "brightnessctl --device=intel_backlight s 10%-"
+brightnessUpBig     = "brightnessctl --device=intel_backlight s +10%"
+brightnessDownSmall = "brightnessctl --device=intel_backlight s 1%-"
+brightnessUpSmall   = "brightnessctl --device=intel_backlight s +1%-"
 
 --  workspaces
 myWorkspaces = ["Main", "Side", "Notes", "Social", "Mail", "System"]
