307f2ac9bc0be7caab7f27440f211d5f17741c28
schimen <einesimen@gmail.com>
2025-12-18 14:34:37 +0100

    Remove git config and add tmux config to simen user

--- home/simen.nix
+++ home/simen.nix
@@ -2,9 +2,11 @@
 {
   users.users.simen = {
     isNormalUser = true;
+    description = "Simen Løcka Eine";
     extraGroups = [
       "wheel" "networkmanager" "dialout" "docker" "kvm" "libvirtd"
     ];
+    packages = [ pkgs.wl-clipboard ];
   };
 
   home-manager.users.simen = {
@@ -12,13 +14,6 @@
     home.keyboard.layout = "no";
     programs = {
       home-manager.enable = true;
-      git = {
-        enable = true;
-        settings.user = {
-          name = "schimen";
-          email = "einesimen@gmail.com";
-        };
-      };
       bash = {
         enable = true;
         shellAliases = {
@@ -29,6 +24,16 @@
         bashrcExtra = ''
           eval "$(direnv hook bash)"
           PS1="\n\[\033[1;32m\]\u@\h\[\033[00m\]:\[\033[1;34m\]\w\[\033[00m\]\$ "
+        '';
+      };
+      tmux = {
+        enable = true;
+        keyMode = "vi";
+        clock24 = true;
+        mouse = true;
+        extraConfig = ''
+          set -s set-clipboard off
+          set -s copy-command 'wl-copy'
         '';
       };
     };
