b518b470819d4d9574e0064804728ea9f1691ed2
schimen <einesimen@gmail.com>
2026-01-15 21:52:20 +0100

    Use new intel gpu settings in media server

--- media-server.nix
+++ media-server.nix
@@ -114,18 +114,11 @@
     zerotierone
   ];
 
-  # Enable VA-API (see https://nixos.wiki/wiki/Jellyfin)
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  };
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
       intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-      intel-compute-runtime
+      intel-ocl
     ];
   };
 
